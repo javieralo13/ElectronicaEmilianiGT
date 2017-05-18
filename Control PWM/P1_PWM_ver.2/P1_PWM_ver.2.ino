f2ebf5d685254030ea8ba1eba2e6db557960690a
javieralo13 <javieralo13@gmail.com>
2017-05-18 00:48:43 -0600

    Primera version
    primesa modificacion para subir las carpetas con los archivos .ino

---
+++ Control PWM/P1_PWM_ver.2/P1_PWM_ver.2.ino
@@ -0,0 +1,42 @@
+/*
+ * TEMAS: este programa contiene una implementación al uso de PWM
+ * entrada de bottones
+ * Desplegar PWM mediante la funcion AnalogWrite()
+ */
+
+const byte pinLED= 7;
+const byte pinMotor = 6;
+
+const byte push1= 0;
+ 
+void setup() {
+  pinMode(pinLED, OUTPUT);
+  pinMode(pinMotor, OUTPUT);
+  pinMode(push1, INPUT_PULLUP);
+
+}
+//variables Globales
+uint8_t contador; // si hay mucho codigo --> uint8_t contador=0;
+
+void loop() {
+  boolean estadoPush = digitalRead(push1);
+
+  if(estadoPush==LOW){
+    digitalWrite(pinLED,HIGH);
+
+    //asignacion valor de PWM
+    analogWrite(pinMotor,contador);
+    contador = contador+17; //** 255/15=17
+    if(contador>=255){
+      contador=0;
+    }
+
+    delay(200);
+    digitalWrite(pinLED,LOW);
+  }
+
+}//fin void loop()
+
+
+
+
